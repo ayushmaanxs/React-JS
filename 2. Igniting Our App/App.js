5b6ac88d9f552b18d7c99cccd2741f6f9a529def
Ayushmaan Shandilya <90828689+ayushmaanxs@users.noreply.github.com>
2024-08-24 20:11:31 +0530

    Episode 2 Igniting Our App

--- 2. Igniting Our App/App.js
+++ 2. Igniting Our App/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 /* 
 
 <div id="parent">
@@ -17,7 +20,7 @@ ReactElement(Object) => HTML(Browser Understands)
 
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, "I'm h1 Tag"),
+    React.createElement('h1', {}, 'This is Namaste React 🚀'),
     React.createElement('h2', {}, "I'm h2 Tag"),
   ]),
   React.createElement('div', { id: 'child2' }, [
